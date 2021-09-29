ebfd41ff346cb5b5b9405b0b4bef41bf64f78af1
Jesse <70843294+jessep92@users.noreply.github.com>
2021-09-28 22:23:02 -0700

    Add form

--- Module 4/calculator.php
+++ Module 4/calculator.php
@@ -7,6 +7,16 @@
     <title>Calculator</title>
 </head>
 <body>
-    <p>Test</p>
+    <h1>Addition calculator</h1>
+    <p>Please enter two numbers to find the sum:</p>
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="num_1">First Number</label>
+        <input type="number" name="num_1" value="" min="0" required>
+        <br><br>
+        <label for="num_2">Second Number</label>
+        <input type="number" name="num_2" value="" min="0" required>
+        <br><br>
+        <input type="submit" value="Calculate Sum">
+    </form>
 </body>
 </html>
